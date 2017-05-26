01697931436514629b5632f79ff8d3a53cc86b5f
7036tom <tom.dagens@centrale-marseille.fr>
2017-05-26 10:05:53 +0200

    gridsearch

--- GridSearch.py
+++ GridSearch.py
@@ -26,7 +26,7 @@ numpy.random.seed(seed)
 
 # Function to create model, required for KerasClassifier
 
-def create_model(dr1, dr2, dr3, dr4):
+def create_model(w1l1, w1l2, w2l1, w2l2, w3l1, w3l2):
     # create model
     #L=WinnerTakeAll1D_GaborMellis(spatial=1, OneOnX=WTAX)
     model = Sequential()
@@ -35,15 +35,14 @@ def create_model(dr1, dr2, dr3, dr4):
 
     model.add(Dense(100, input_dim=95, init='normal', activation='relu', W_regularizer=l1l2(l1=0.0005, l2=0.0001)))# ,W_regularizer=l1l2(l1=9E-7, l2=5e-07))) #W_regularizer=l1(0.000001), activity_regularizer=activity_l1(0.000001)))
     #model.add(L)
-    model.add(Dropout(dr1))
-    model.add(Dense(540, activation ='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(540, activation ='relu', W_regularizer=l1l2(l1=w1l1, l2=0) ))
     #model.add(L)
-    model.add(Dropout(dr2))
-    model.add(Dense(310, activation ='relu'))
-    #model.add(L)
-    model.add(Dropout(dr3))
-    model.add(Dense(440, activation ='relu'))
-    model.add(Dropout(dr4))
+    model.add(Dropout(0.3))
+    model.add(Dense(310, activation ='relu')) 
+    model.add(Dropout(0.5))
+    model.add(Dense(440, activation ='relu', W_regularizer=l1l2(l1=0, l2=w3l2)))
+    model.add(Dropout(0.4))
     model.add(Dense(2))
     model.add(Activation('softmax'))
     admax = Adamax(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0)
@@ -52,16 +51,16 @@ def create_model(dr1, dr2, dr3, dr4):
 
 
 # load dataset
-dataframe = pandas.read_csv("Database.csv", header=None)
+dataframe = pandas.read_csv("Database_95_var.csv", header=None)
 dataset = dataframe.values
 
 np.random.shuffle(dataset)
 
-X1 = dataset[0:100000,2:17].astype(float)
-X2 = dataset[0:100000,22:102].astype(float)
+X1 = dataset[0:50000,2:17].astype(float)
+X2 = dataset[0:50000,22:102].astype(float)
 X = np.hstack([X1, X2])
 
-Y = dataset[0:100000,0:1].astype(float)
+Y = dataset[0:50000,0:1].astype(float)
 
 
 # Preprocessing
@@ -88,6 +87,14 @@ neurons1 = [70, 100, 120, 150]
 neurons2 = [450, 480, 510, 540]
 neurons3 = [250, 280, 310, 340]
 neurons4 = [450, 480, 510, 540]
+
+w1l1 = [0, 0.0001, 0.0005 ,0.00001, 0.00005, 0.000001, 0.000005, 0.0000001, 0.0000005]
+w1l2 = [0, 0.00001]
+w2l1 = [0, 0.00001]
+w2l2 = [0, 0.00001]
+w3l1 = [0, 0.00001]
+w3l2 = [0, 0.0001, 0.0005 ,0.00001, 0.00005, 0.000001, 0.000005, 0.0000001, 0.0000005]
+
 batch_size = [80,83,86,89,92,95,98, 100]
 epochs = [100, 120, 140, 160, 180, 200]
 WTAX=[3,4,5]
@@ -96,8 +103,8 @@ l2_value = [0, 0.0001, 0.0005 ,0.00001, 0.00005, 0.000001, 0.000005, 0.0000001, 
 l_rate = [0.001, 0.002, 0.003 ,0.004]
 decay = [0.01, 0.001, 0.0001, 0.00001, 0.000001, 0]
 
-param_grid = dict(dr1=dr1, dr2=dr2, dr3=dr3, dr4=dr4)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv =None)#, verbose=1)
+param_grid = dict(w1l1=w1l1, w1l2=w1l2, w2l1=w2l1, w2l2=w2l2, w3l1=w3l1, w3l2=w3l2)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv =2)#, verbose=1)
 
 grid_result = grid.fit(X, Y)
 # summarize results
